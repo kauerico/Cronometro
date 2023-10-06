d01bf43f6f603b0286229b09b16b01496018700e
Kaue Ribeiro Costa <ribeirokaue70@gmail.com>
2023-10-06 00:06:57 -0400

    Adicionado funcionalidade de Start

--- códico/Cronometro.java
+++ códico/Cronometro.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Cronometro{
+public class Cronometro implements ActionListener {
     ImageIcon logo = new ImageIcon(getClass().getClassLoader().getResource("Imagem/icon.png"));
     JFrame janelaCronometro = new JFrame();
     JLabel contador = new JLabel();
@@ -21,36 +21,37 @@ public class Cronometro{
     String horasTexto = String.format("%02d", horas);
 
     Timer timer = new Timer(1000, new ActionListener() {
-  
-        public void actionPerformed(ActionEvent e) {
-         
-         milissegundos=milissegundos+1000;
-         horas = (milissegundos/3600000);
-         minutos = (milissegundos/60000) % 60;
-         segundos = (milissegundos/1000) % 60;
-         segundosTexto = String.format("%02d", segundos);
-         minutosTexto = String.format("%02d", minutos);
-         horasTexto = String.format("%02d", horas);
-         contador.setText(horasTexto+":"+minutosTexto+":"+segundosTexto);
-         
-        }
-        
-       });
 
-    Cronometro(){
-        contador.setText(horasTexto+ ":"+ minutosTexto + ":" + segundosTexto);
-        contador.setBounds(100,100,200,100);
-        contador.setFont(new Font("Verdana", Font.TYPE1_FONT, 35));
+        public void actionPerformed(ActionEvent e) {
+
+            milissegundos = milissegundos + 1000;
+            horas = (milissegundos / 3600000);
+            minutos = (milissegundos / 60000) % 60;
+            segundos = (milissegundos / 1000) % 60;
+            segundosTexto = String.format("%02d", segundos);
+            minutosTexto = String.format("%02d", minutos);
+            horasTexto = String.format("%02d", horas);
+            contador.setText(horasTexto + ":" + minutosTexto + ":" + segundosTexto);
+
+        }
+
+    });
+
+    Cronometro() {
+        contador.setText(horasTexto + ":" + minutosTexto + ":" + segundosTexto);
+        contador.setBounds(100, 100, 200, 100);
+        contador.setFont(new Font("Verdana", Font.PLAIN, 35));
         contador.setBorder(BorderFactory.createBevelBorder(1));
         contador.setOpaque(true);
-        contador.setHorizontalAlignment(JTextField.HORIZONTAL);
+        contador.setHorizontalAlignment(JTextField.CENTER);
 
-        startButton.setBounds(100,230 ,100, 50);
-        startButton.setFont(new Font("Ink free",Font.PLAIN, 20));
+        startButton.setBounds(100, 230, 100, 50);
+        startButton.setFont(new Font("Ink free", Font.PLAIN, 20));
         startButton.setFocusable(false);
+        startButton.addActionListener(this);
 
-        resetButton.setBounds(200,230 ,100, 50);
-        resetButton.setFont(new Font("Ink free",Font.PLAIN, 20));
+        resetButton.setBounds(200, 230, 100, 50);
+        resetButton.setFont(new Font("Ink free", Font.PLAIN, 20));
         resetButton.setFocusable(false);
 
         janelaCronometro.add(resetButton);
@@ -60,30 +61,30 @@ public class Cronometro{
         janelaCronometro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         janelaCronometro.setSize(420, 420);
         janelaCronometro.setResizable(false);
-        janelaCronometro.setLocationRelativeTo(null);  
-        janelaCronometro.getContentPane().setBackground(new Color(0,0,0) );
+        janelaCronometro.setLocationRelativeTo(null);
+        janelaCronometro.getContentPane().setBackground(new Color(0, 0, 0));
         janelaCronometro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         janelaCronometro.setTitle("Cronômetro");
         janelaCronometro.setIconImage(logo.getImage());
 
     }
-    public void actionPerformed(ActionEvent e){
-        if (e.getSource() == startButton){
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == startButton) {
             Start();
         }
+
     }
 
     void Start() {
         timer.start();
     }
-    
 
-    void Stop(){
+    void Stop() {
 
     }
 
-    void Reset(){
+    void Reset() {
 
     }
 }
-
